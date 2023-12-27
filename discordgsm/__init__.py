ebcbba856bf726045b2223296a3525cc98f916bd
Percy <percy@railway.app>
2023-12-27 09:19:09 +0000

    Initial Commit

---
+++ discordgsm/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa
+from discordgsm.environment import env
+from discordgsm.logger import Logger
+from discordgsm.version import __version__
---
+++ tests/test_import.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+def test_import():
+    import discordgsm.styles
+    import discordgsm.database
+    import discordgsm.gamedig
+    import discordgsm.logger
+    import discordgsm.main
+    import discordgsm.server
+    import discordgsm.service
